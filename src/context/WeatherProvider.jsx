d198c73eccbc37d175502b497a8e348e94206c83
Inti Silva <inti.t.silva@gmail.com>
2024-04-09 17:49:50 -0300

    ..

--- src/context/WeatherProvider.jsx
+++ src/context/WeatherProvider.jsx
@@ -29,12 +29,12 @@ const WeatherProvider = ({children}) => {
 
             const appId = import.meta.env.VITE_API_KEY
 
-            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`
+            const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`
 
             const { data } = await axios(url)
             const { lat, lon } = data[0]
 
-            const urlWeather = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+            const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
             const { data: weather } = await axios(urlWeather)
             setResult(weather)
